be93d6f399bfbb65b8ce07c2a4d3ef518ef87ef4
Aether <140750964+this-progammer@users.noreply.github.com>
2026-08-27 08:29:43 -0500

    Update tooler_env.py

--- dev/tooler_env.py
+++ dev/tooler_env.py
@@ -16,8 +16,18 @@ class ToolerEnvironment :
   def __init__( self ):
     return self
 
+  """*set_tooler_env_path*"""
   def set_tooler_env_path( path : str ):
     TOOLER_PATH = path
 
+  """*set_tooler_source_files*"""
   def set_tooler_source_files( files : str, ext : str ):
     TOOLER_SOURCE_FILES = [files, ext]
+
+  """*tooler_env_path_clear*"""
+  def tooler_env_path_clear():
+    TOOLER_PATH = ''
+
+  """tooler_env_source_clear*"""
+  def tooler_env_source_clear():
+    TOOLER_SOURCE_FILES = ''
